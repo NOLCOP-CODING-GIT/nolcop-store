75d88e6821479c6bf53a6aef23828b00ec9e19f5
Jean Orland Zinsou <jeanorlandzinsou@gmail.com>
2026-01-12 20:44:23 +0100

    Fix image path in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link to="/" className="flex items-center space-x-3 group">
               <img
-                src="/public/profile-photo.png"
+                src="/profile-photo.png"
                 alt="Logo"
                 className="w-12 h-12 rounded-full object-cover"
               />
